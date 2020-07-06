a655e64a6449aa68c9a1ab82dd845599fb4cd2d7
OmidGheysar <omid.geysar@gmail.com>
2020-07-05 20:06:53 -0700

    Plot maker improve some missing value in quartile was making trouble

--- Design Dashboard.R
+++ Design Dashboard.R
@@ -100,6 +100,9 @@ server <- function(input, output) {
                              input$day)),
       stringsAsFactors = FALSE)
     
+
+    
+    
   })
   
   # Show the values in an HTML table ----
@@ -119,47 +122,8 @@ server <- function(input, output) {
       
       output<- select100Scenarios(dat,2,0,0,.8,1,5,.8)
     
-     output<- select100Scenarios(input$R0,
-                                 input$p.trace,
-                                 input$p.trace_app,
-                                 input$p.symp,
-                                 input$iso_delay_traced_max,
-                                 input$iso_delay_untraced_sd_max,
-                                 input$sd_contact_rate1)
-     
-     
       
-      dB <- data.frame(RQ2=double(),RQ3=double(),RQ4=double(),n.active=double(), stringsAsFactors=FALSE)
-      for (i in 0:31){
-        result<- output %>% filter(day==i) %>% select("day","Rt","n.active")
-        dB[nrow(dB) + 1,] = c(quantile(result$Rt)[c(2,3,4)],mean(result$n.active))
-      }
       
-      days <- 0:31
-      plot(days, dB$RQ3)
-      
-      quantile(output$Rt)[c(2,3,4)]
-      p <- ggplot(data = dB, mapping =  aes(x = days, y = RQ3))
-      
-      p <- p + geom_point()
-      p <- p + geom_line()
-      p
-      
-      myDay <- (0:31)
-      Q2 <- dB$RQ2
-      Q3 <- dB$RQ3
-      Q4 <- dB$RQ4
-      kB <- tibble(myDay,Q2, Q3, Q4)
-      # kB <- cbind(kB, as.double(0:31) )
-      kB <- kB %>% pivot_longer(cols = c(Q2, Q3, Q4),
-                                names_to = "Activity")
-      
-      ggplot(kB,aes(x = myDay,
-                    y = value,
-                    col = Activity,
-                    group = Activity)) +
-        geom_line() +
-        geom_point() +ylim(0,2.8)
       
       
       
---
+++ PlotMaker.R
@@ -0,0 +1,59 @@
+
+library("ggplot2")
+library(magrittr) # needs to be run every time you start R and want to use %>%
+library(dplyr)    # alternatively, this also loads %>%
+library("tidyverse")
+source("C:/Users/omidg/OneDrive/Desktop/BCCCDC R shiny Project/BCCCDC-Project/data manipulation.R")
+
+dat <- readRDS("05_22.rds")
+returnPlot <- function(datOutput,
+                       R0,
+                       p.trace,
+                       p.trace_app,
+                       p.symp,
+                       iso_delay_traced_max,
+                       iso_delay_untraced_sd_max,
+                       sd_contact_rate1) {
+  
+  output<- select100Scenarios(dat,
+                              R0,
+                              p.trace,
+                              p.trace_app,p.symp,
+                              iso_delay_traced_max,
+                              iso_delay_untraced_sd_max,
+                              sd_contact_rate1)
+  
+  
+  dB <- data.frame(RQ2=double(),RQ3=double(),RQ4=double(), stringsAsFactors=FALSE)
+  for (i in 0:31){
+    myResult<- output %>% filter(day==i) %>% select("day","Rt")
+    Q <-  quantile(myResult$Rt,na.rm = TRUE)
+    dB[nrow(dB) + 1,] = c(Q[c(2,3,4)])
+  }
+
+  myDay <- (0:31)
+  Q2 <- dB$RQ2
+  Q3 <- dB$RQ3
+  Q4 <- dB$RQ4
+  kB <- tibble(myDay,Q2, Q3, Q4)
+  kB <- kB %>% pivot_longer(cols = c(Q2, Q3, Q4),
+                            names_to = "Activity")
+
+  plotOut <- ggplot(kB,aes(x = myDay,
+                y = value,
+                col = Activity,
+                group = Activity)) +
+    geom_line() +
+    geom_point() +ylim(0,2.8)
+
+  return(plotOut)
+}
+myPlot<- returnPlot(dat, 2.5,.5,.5,.7,2,1,.3)
+myPlot
+
+
+
+
+ 
+
+
