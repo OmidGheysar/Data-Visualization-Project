137177f730c33346dc24436d3b193c90dbe8233b
OmidGheysar <omid.geysar@gmail.com>
2020-07-15 20:18:03 -0700

    First version of Dashboard which upload data set inside a server!

--- MadulatedDashboard.R
+++ MadulatedDashboard.R
@@ -1,7 +1,6 @@
-# source("C:/Users/omidg/OneDrive/Desktop/BCCCDC R shiny Project/BCCCDC-Project/uploadRequiredLibraries.R")
-# uploadRequiredLibraries()
+source("C:/Users/omidg/OneDrive/Desktop/BCCCDC R shiny Project/BCCCDC-Project/uploadRequiredLibraries.R")
+uploadRequiredLibraries()
 
-# dat <- readRDS("05_22.rds")
 
 header <- dashboardHeader(title = "COVID-19 Simulation Dashboard")
 
@@ -49,28 +48,74 @@ ui <- dashboardPage(header, sidebar, body)
 
 server <- function (input, output, session){
   
+  dat <- readRDS("05_22.rds")
+  
       output$plotRtTime <- renderPlot({
-        plotRtTime(input)
-        
+        outputPlot<- returnPlot(dat,"Rt",
+                                input$R0,
+                                input$p.trace,
+                                input$p.trace_app,
+                                input$p.symp,
+                                input$iso_delay_traced_max,
+                                input$iso_delay_untraced_sd_max,
+                                input$sd_contact_rate1)
+        outputPlot
       })
       
       output$plotRtNactive <- renderPlot({
-        plotRtNactive(input)
-        
+        outputPlot<- returnPlot(dat,"n.active",
+                                input$R0,
+                                input$p.trace,
+                                input$p.trace_app,
+                                input$p.symp,
+                                input$iso_delay_traced_max,
+                                input$iso_delay_untraced_sd_max,
+                                input$sd_contact_rate1)
+        outputPlot
       })
       
       output$plotRt_Only_App <- renderPlot({
-        plotRt_Only_App(input)
+        
+        myPlot <- RtBasedonAppTrace(dat,
+                                    day = input$daysforApp,
+                                    R = input$R0forApp,
+                                    p.tr = 0,
+                                    p.trace_ap = 100,
+                                    p.sym = input$p.symforApp,
+                                    iso_delay_traced=1,
+                                    iso_delay_untraced= input$iso_delay_untracedforApp,
+                                    sd_contact = input$sd_contactforApp)
+        myPlot
 
       })
       
       output$plotRt_Only_Manual <- renderPlot({
-        plotRt_Only_Manual(input)
+        
+        myPlot <- RtBasedonManualTrace(dat,
+                                       day = input$daysforManual,
+                                       R = input$R0forManual,
+                                       p.tr = 100,
+                                       p.trace_ap = 0,
+                                       p.sym = input$p.symforManual,
+                                       iso_delay_traced=100,
+                                       iso_delay_untraced= input$iso_delay_untracedforManual,
+                                       sd_contact = input$sd_contactforManual)
+        myPlot
         
       })
       
       output$plotRt_App_Manual <- renderPlot({
-        plotRt_App_Manual(input)
+        
+        myPlot <- RtBasedonAppAndManual(dat,
+                                        day = input$daysforAppManual,
+                                        R = input$R0forAppManual,
+                                        p.tr = 100,
+                                        p.trace_ap = 100,
+                                        p.sym = input$p.symforAppManual,
+                                        iso_delay_traced=input$iso_delay_tracecedforAppManual,
+                                        iso_delay_untraced= input$iso_delay_untracedforAppManaual,
+                                        sd_contact = input$sd_contactforAppManual)
+        myPlot
         
       })
  
