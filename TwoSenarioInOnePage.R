4304ea5b8f1b1f725331db99c2ff8e230c8c5b5d
OmidGheysar <omid.geysar@gmail.com>
2020-07-20 12:10:32 -0700

    having two scenarios in the R shiny

--- TwoSenarioInOnePage.R
+++ TwoSenarioInOnePage.R
@@ -16,25 +16,15 @@ source("C:/Users/omidg/OneDrive/Desktop/BCCCDC R shiny Project/BCCCDC-Project/sl
 ui <- fluidPage(
   fluidRow(
     column(3,
-           
-           # Copy the line below to make a slider bar
-           # sliderInput("slider1", label = h3("Slider"), min = 0,
-           #             max = 100, value = 50)
            slidebar1For2()
     ),
     column(6,
-           
-           # Copy the line below to make a slider range 
-           plotOutput("plot"),
-           plotOutput("plot1")
+           plotOutput("plot1"),
+           plotOutput("plot2")
            
     ), 
     column(3,
-           
-           # Copy the line below to make a slider range 
-           # sliderInput("slider2", label = h3("Slider Range"), min = 0, 
-           #             max = 100, value = c(40, 60))
-           slidebar1For2()
+           slidebar1For3()
     )
     
   ),
@@ -54,20 +44,115 @@ ui <- fluidPage(
 
 server <- function(input, output) {
   
-  # You can access the value of the widget with input$slider1, e.g.
-  # output$value <- renderPrint({ input$day })
-  # 
-  # # You can access the values of the second widget with input$slider2, e.g.
-  # output$range <- renderPrint({ input$day })
-  # 
-  # output$plot <- renderPlot({
-  #   output1 <- serverDesign(dat,"Rt",input)
-  # })
-  # 
-  # output$plot1 <- renderPlot({
-  #   output <- serverDesign(dat,"n.active",input)
-  #   output
-  # })
+  output$plot1 <- renderPlot({
+  
+  # scenarios<- select100Scenarios(dat, 2,.5,.5,.7,2,1,.3)
+  scenarios<- select100Scenarios(dat,
+                                 input$R012,
+                                 input$p.trace12,
+                                 input$p.trace_app12,
+                                 input$p.symp12,
+                                 input$iso_delay_traced_max12,
+                                 input$iso_delay_untraced_sd_max12,
+                                 input$sd_contact_rate112)
+  yAxsis <- "Rt"
+
+
+  results1 <- scenarios%>%
+    select(day,yAxsis) %>%
+    group_by(day) %>%
+    summarise(
+      Q_05 = quantile(get(yAxsis), 0.05, na.rm=TRUE),
+      Q_25 = quantile(get(yAxsis), 0.25, na.rm=TRUE),
+      Rt_median = median(get(yAxsis), na.rm=TRUE),
+      Q_75 = quantile(get(yAxsis), 0.75, na.rm=TRUE),
+      Q_90 = quantile(get(yAxsis), 0.90, na.rm=TRUE)
+    ) %>% ungroup()
+
+  paired.cols <- RColorBrewer::brewer.pal(12, "Paired")
+  p <- ggplot2::ggplot(results1, ggplot2::aes(x=day)) +
+    ggplot2::geom_ribbon(
+      ggplot2::aes(y=Rt_median,
+                   ymin=Q_05,
+                   ymax=Q_90),
+      fill=paired.cols[7],
+      alpha=0.2
+    ) +
+    ggplot2::geom_ribbon(
+      ggplot2::aes(y=Rt_median,
+                   ymin=Q_25,
+                   ymax=Q_75),
+      fill=paired.cols[8],
+      alpha=0.3
+    ) +
+    ggplot2::geom_line(
+      ggplot2::aes(y=Rt_median),
+      color=paired.cols[8],
+      size=1.2
+    ) +
+    ggplot2::geom_point(
+      ggplot2::aes(y=Rt_median),
+      color=paired.cols[8],
+      size=3
+    )
+
+
+
+
+
+  scenarios<- select100Scenarios(dat,
+                                 input$R023,
+                                 input$p.trace23,
+                                 input$p.trace_app23,
+                                 input$p.symp23,
+                                 input$iso_delay_traced_max23,
+                                 input$iso_delay_untraced_sd_max23,
+                                 input$sd_contact_rate123)
+  yAxsis <- "Rt"
+
+
+  results2 <- scenarios%>%
+    select(day,yAxsis) %>%
+    group_by(day) %>%
+    summarise(
+      Q_05 = quantile(get(yAxsis), 0.05, na.rm=TRUE),
+      Q_25 = quantile(get(yAxsis), 0.25, na.rm=TRUE),
+      Rt_median = median(get(yAxsis), na.rm=TRUE),
+      Q_75 = quantile(get(yAxsis), 0.75, na.rm=TRUE),
+      Q_90 = quantile(get(yAxsis), 0.90, na.rm=TRUE)
+    ) %>% ungroup()
+
+  p <- p+ geom_ribbon(aes(y=results2$Rt_median,
+                          ymin=results2$Q_05,
+                          ymax=results2$Q_90),
+                      fill=paired.cols[2],
+                      alpha=0.2
+  )+
+    ggplot2::geom_ribbon(
+      ggplot2::aes(y=results2$Rt_median,
+                   ymin=results2$Q_25,
+                   ymax=results2$Q_75),
+      fill=paired.cols[2],
+      alpha=0.3
+    ) +
+    ggplot2::geom_line(
+      ggplot2::aes(y=results2$Rt_median),
+      color=paired.cols[2],
+      size=1.2
+    ) +
+    ggplot2::geom_point(
+      ggplot2::aes(y=results2$Rt_median),
+      color=paired.cols[2],
+      size=3
+    )
+
+
+    p
+  })
+
+  output$plot2 <- renderPlot({
+    p
+  })
 }
 
 
--- slidebar1For2.R
+++ slidebar1For2.R
@@ -2,45 +2,87 @@ slidebar1For2 <- function() {
   
   ui <- fluidPage(
 
-sliderInput("R0", "R0",
-            min = 2, max = 3,
-            value = 2.5, step = 0.5),
-
-        sliderInput("p.trace", "p.trace",
-                    min = 0, max = 1,
-                    value = .5, step = 0.25),
-
-        sliderInput("p.trace_app", "p.trace_app",
-                    min = 0, max = 1,
-                    value = .5, step = 0.25),
-
-        sliderInput("p.symp", "p.symp",
-                    min = 0.6, max = 0.8,
-                    value = .7, step = 0.1),
-
-        sliderInput("iso_delay_traced_max", "iso",
-                    min = 1, max = 4,
-                    value = 2, step = 1),
-
-        sliderInput("iso_delay_untraced_sd_max", "iso_delay",
-                    min = 1, max = 5,
-                    value = 1, step = 4),
-    #     # 
-        sliderInput("sd_contact_rate1", "sd_contact_rate1",
-                    min = 0.3, max = 0.8,
-                    value = 0,3, , step = .5),
-        shinyWidgets::sliderTextInput("sd_contact_rate1","sd_contact",
-                                      choices=c(0.3, 0.6, 0.8),
-                                      selected=0.3, grid = T),
-
-        sliderInput("day", "day",
-                    min = 0, max = 31,
-                    value = 20, step = 1)
+    # Input: Simple integer interval ----
+    sliderInput("R012", "R0",
+                min = 2, max = 3,
+                value = 2.5, step = 0.5),
+    
+    sliderInput("p.trace12", "Fraction of cases manually traced",
+                min = 0, max = 1,
+                value = .5, step = 0.25),
+    
+    sliderInput("p.trace_app12", "Fraction of people using contact tracing app",
+                min = 0, max = 1,
+                value = .5, step = 0.25),
+    
+    sliderInput("p.symp12", "Fraction of cases that are symptomatic",
+                min = 0.6, max = 0.8,
+                value = .7, step = 0.1),
+    
+    sliderInput("iso_delay_traced_max12", "Delay to isolation for traced cases (days)",
+                min = 1, max = 4,
+                value = 2, step = 1),
+    
+    sliderInput("iso_delay_untraced_sd_max12", "Delay to isolation for untraced & distancing cases",
+                min = 1, max = 5,
+                value = 1, step = 4),
+    
+    # sliderInput("sd_contact_rate1", "sd_contact_rate1",
+    #             min = 0.3, max = 0.8,
+    #             value = 0,3, , step = .5),
+    shinyWidgets::sliderTextInput("sd_contact_rate112","Strength of physical distancing (contact rate)",
+                                  choices=c(0.3, 0.6, 0.8),
+                                  selected=0.3, grid = T),
+    
+    sliderInput("day", "day",
+                min = 0, max = 31,
+                value = 20, step = 1)
   )
   return(ui)
 }
 
-
+slidebar1For3 <- function() {
+  
+  ui <- fluidPage(
+    
+    # Input: Simple integer interval ----
+    sliderInput("R023", "R0",
+                min = 2, max = 3,
+                value = 2.5, step = 0.5),
+    
+    sliderInput("p.trace23", "Fraction of cases manually traced",
+                min = 0, max = 1,
+                value = .5, step = 0.25),
+    
+    sliderInput("p.trace_app23", "Fraction of people using contact tracing app",
+                min = 0, max = 1,
+                value = .5, step = 0.25),
+    
+    sliderInput("p.symp23", "Fraction of cases that are symptomatic",
+                min = 0.6, max = 0.8,
+                value = .7, step = 0.1),
+    
+    sliderInput("iso_delay_traced_max23", "Delay to isolation for traced cases (days)",
+                min = 1, max = 4,
+                value = 2, step = 1),
+    
+    sliderInput("iso_delay_untraced_sd_max23", "Delay to isolation for untraced & distancing cases",
+                min = 1, max = 5,
+                value = 1, step = 4),
+    
+    # sliderInput("sd_contact_rate1", "sd_contact_rate1",
+    #             min = 0.3, max = 0.8,
+    #             value = 0,3, , step = .5),
+    shinyWidgets::sliderTextInput("sd_contact_rate123","Strength of physical distancing (contact rate)",
+                                  choices=c(0.3, 0.6, 0.8),
+                                  selected=0.3, grid = T),
+    
+    sliderInput("day", "day",
+                min = 0, max = 31,
+                value = 20, step = 1)
+  )
+  return(ui)
+}
 
 
 
