da7f2e564818d2d7d0395b62ca8c5e8e5e258751
OmidGheysar <omid.geysar@gmail.com>
2020-07-29 18:19:47 -0700

    fourth page is updated with new Data set!!

--- RtaccordingToTraceApp.R
+++ RtaccordingToTraceApp.R
@@ -10,7 +10,7 @@ RtBasedonAppTrace <- function(dat,
                                  sd_contact){
 
 
-selectScenarios <- function(dat,
+select64000Scenarios <- function(dat,
                             days,
                             R,
                             p.tr,
@@ -21,27 +21,28 @@ selectScenarios <- function(dat,
                             sd_contact) {
   
   scenarios<-dat %>% filter(R0==R &
-                              p.trace==p.tr&
+                              p.trace==0&
                               # p.trace_app==p.trace_ap&
                               p.symp== p.sym&
                               iso_delay_traced_max==iso_delay_traced&
                               # iso_delay_untraced_sd_max==iso_delay_untraced&
                               sd_contact_rate1==sd_contact) %>% filter(day==days) %>%  
-    select(p.trace_app,iso_delay_untraced_sd_max,"day":"Rt") 
+    select(p.trace_app,iso_delay_untraced_sd_max,"day":"n.active_Q_95") 
   return(scenarios)
 }
 
 
 aes_x <- "p.trace_app"
-aes_y <- "Rt"
+aes_y <- "Revenue"
 aes_col <- "iso_delay_untraced_sd_max"
 aes_grp <- "iso_delay_untraced_sd_max"
 
-# results<- selectScenarios(dat, 5,3,0,.5,.7,2,1,.3)
-results<- selectScenarios(dat, days, R, p.tr, p.trace_ap, p.sym,
+results<- select64000Scenarios(dat, days, R, p.tr, p.trace_ap, p.sym,
                           iso_delay_traced, iso_delay_untraced, sd_contact)
- 
-p <- ggplot(results,
+# dat <- readRDS("Newdata.rds")
+# results<- select64000Scenarios(dat,10 ,3,.5,.5,.7,2,1,.3)
+long_results <- results %>% gather(Quarter, Revenue, Rt_Q_05:Rt_Q_95) 
+p <- ggplot(long_results,
             aes(x=eval(as.name(aes_x)),
                 y=eval(as.name(aes_y)),
                 color=factor(eval(as.name(aes_col))),
