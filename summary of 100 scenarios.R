60d805a1f277ae4502af289469ca42129185b069
OmidGheysar <omid.geysar@gmail.com>
2020-08-25 01:34:16 -0700

    Add required data to be ploted!!

--- summary of 100 scenarios.R
+++ summary of 100 scenarios.R
@@ -9,14 +9,7 @@ dat <- readRDS("05_22.rds")
 
 result <- dat %>% group_by(day,scn_id) %>% summarise(n=n())
 saveRDS(result, file = "my_data.rds")
-# result <- dat %>% group_by(day,scn_id)%>% 
-#   summarise(Rt_Q_05 =quantile(Rt, 0.05, na.rm=TRUE),
-#             Rt_Q_05 =quantile(Rt, 0.25, na.rm=TRUE),
-#             Rt_Q_05 =quantile(Rt, 0.5, na.rm=TRUE),
-#             Rt_Q_05 =quantile(Rt, 0.75, na.rm=TRUE),
-#             Rt_Q_05 =quantile(Rt, 0.5, na.rm=TRUE),
-#             Rt_Q_05 =quantile(Rt, 0.5, na.rm=TRUE),
-#             Rt_Q_05 =quantile(Rt, 0.5, na.rm=TRUE),)
+
 
 result <- dat %>% group_by(R0,
                            p.trace,
@@ -29,7 +22,16 @@ result <- dat %>% group_by(R0,
                            scn_id)%>%
   summarise(n=n())
 
-
+# enhanced data
+dat <- readRDS("05_22.rds")
+addColum <- dat %>% summarise(n.new =n.new_S+n.new_I)
+dat <- data.frame(dat,addColum)
+saveRDS(dat, file = "enhanced_data.rds")
+# Starts from here
+library(magrittr) # needs to be run every time you start R and want to use %>%
+library(dplyr)    # alternatively, this also loads %>%
+library("tidyverse")
+dat <- readRDS("enhanced_data.rds")
 result <- dat %>% group_by(R0,
                            p.trace,
                            p.trace_app, 
@@ -44,11 +46,31 @@ result <- dat %>% group_by(R0,
             Rt_Q_50 =quantile(Rt, 0.50, na.rm=TRUE),
             Rt_Q_75 =quantile(Rt, 0.75, na.rm=TRUE),
             Rt_Q_95 =quantile(Rt, 0.95, na.rm=TRUE),
+            
             n.active_Q_05 =quantile(n.active, 0.05, na.rm=TRUE),
             n.active_Q_25 =quantile(n.active, 0.25, na.rm=TRUE),
             n.active_Q_50 =quantile(n.active, 0.50, na.rm=TRUE),
             n.active_Q_75 =quantile(n.active, 0.75, na.rm=TRUE),
-            n.active_Q_95 =quantile(n.active, 0.95, na.rm=TRUE))
+            n.active_Q_95 =quantile(n.active, 0.95, na.rm=TRUE),
+            
+            n.new_Q_05 =quantile(n.new, 0.05, na.rm=TRUE),
+            n.new_Q_25 =quantile(n.new, 0.25, na.rm=TRUE),
+            n.new_Q_50 =quantile(n.new, 0.50, na.rm=TRUE),
+            n.new_Q_75 =quantile(n.new, 0.75, na.rm=TRUE),
+            n.new_Q_95 =quantile(n.new, 0.95, na.rm=TRUE),
+            
+            n.total_Q_05 =quantile(n.total, 0.05, na.rm=TRUE),
+            n.total_Q_25 =quantile(n.total, 0.25, na.rm=TRUE),
+            n.total_Q_50 =quantile(n.total, 0.50, na.rm=TRUE),
+            n.total_Q_75 =quantile(n.total, 0.75, na.rm=TRUE),
+            n.total_Q_95 =quantile(n.total, 0.95, na.rm=TRUE),
+            
+            n.iso_Q_05 =quantile(n.iso, 0.05, na.rm=TRUE),
+            n.iso_Q_25 =quantile(n.iso, 0.25, na.rm=TRUE),
+            n.iso_Q_50 =quantile(n.iso, 0.50, na.rm=TRUE),
+            n.iso_Q_75 =quantile(n.iso, 0.75, na.rm=TRUE),
+            n.iso_Q_95 =quantile(n.iso, 0.95, na.rm=TRUE),
+            )
 
 saveRDS(result, file = "Newdata.rds")
 result <- readRDS("Newdata.rds")
